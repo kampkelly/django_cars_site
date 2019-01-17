7b585bee1bd176e007ee0be0404f3203097a6bbb
Runor Adjekpiyede <kampkellykeys@gmail.com>
2019-01-17 20:18:41 +0100

    ft(connect-database): connect a postgres database

--- cars_site/cars/models.py
+++ cars_site/cars/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    colour = models.CharField(max_length=200,default='black')
+    model = models.CharField(max_length=200,null=False)
+    name = models.CharField(max_length=200,unique=True,null=False)
+    year = models.IntegerField(null=False)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200,null=False)
+    email = models.CharField(max_length=200,null=False)
+    password = models.CharField(max_length=255,null=False)
--- cars_site/cars_site/settings.py
+++ cars_site/cars_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cars.apps.CarsConfig',
 ]
 
 MIDDLEWARE = [
@@ -72,11 +73,17 @@ WSGI_APPLICATION = 'cars_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_cars',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'mytestdatabase',
+        },
     }
 }
 
@@ -105,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Lagos'
 
 USE_I18N = True
 
